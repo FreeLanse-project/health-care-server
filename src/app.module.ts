36624a49eccb6494a38f58a3cdb19ebfa1e9467f
ZeyadAzzam <ziadazam2001@gmail.com>
2024-10-19 13:09:01 +0300

    setup auth and api key and enviroment

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { APIKeyGuard } from './utils/guards/api-key.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: APIKeyGuard,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/configurations/configuration.interface.ts
@@ -0,0 +1,8 @@
+export interface EnvVariables {
+  ENV: string;
+
+  URL: string;
+  PORT: number;
+
+  API_KEY: string;
+}
